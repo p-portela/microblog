b320710e057c4f4a02b7bdfb8168054824aa2657
Pedro Portela <pportela@disroot.org>
2021-11-21 23:40:52 +0100

    Initial Commit

---
+++ app/routes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import render_template, flash, redirect
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user = {'username': 'Pedro'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie fucking sucked!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts) 
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
